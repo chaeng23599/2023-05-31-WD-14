30964742ceed96835a0b7873f9a9bc5544b543cf
chaeng23599 <yunchaeng@naver.com>
2023-05-31 16:59:02 +0900

    55

--- script/common.js
+++ script/common.js
@@ -1,34 +1,10 @@
 $(function(){
-    //메뉴
-    // $('.gnb li').hover(
-    //     function(){
-    //         $(this).find('ul').stop().slideDown()
-    //     },
-    //     function(){
-    //         $(this).find('ul').stop().slideUp()
-    //     },
-    // )
-        //슬라이드
-    // setInterval(function(){
-    //     $('.slide').animate({ left: '-100%' }, function(){
-    //         $('.slide a:first').appendTo('.slide')
-    //         $('.slide').css('left',0)
-    //     })
-    // },3000)
-
     $('.gnb li').hover(
         function(){
-            $(this).find('ul').stop().slideDown()
+            $(this).find('ul').stop().show()
         },
         function(){
-            $(this).find('ul').stop().slideUp()
-        },
+            $(this).find('ul').stop().hide()
+        }
     )
-
-    setInterval(function(){
-        $('.slide').animate({ left: '-100%'}, function(){
-            $('.slide a:first').appendTo('.slide')
-            $('.slide').css('left',0)
-        })
-    },600000)
 })
